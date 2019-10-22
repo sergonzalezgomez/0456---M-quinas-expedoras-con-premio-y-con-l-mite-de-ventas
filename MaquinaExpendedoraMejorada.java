1dacaae01554cd08dd14171360bb38b2be66dcb8
sergonzalezgomez <43619335+sergonzalezgomez@users.noreply.github.com>
2019-10-22 12:02:14 +0200

    Añadido el metodo vaciarDineroDeLaMaquina que vacia todo el dinero que esta acumulado en ella

--- MaquinaExpendedoraMejorada.java
+++ MaquinaExpendedoraMejorada.java
@@ -85,4 +85,11 @@ public class MaquinaExpendedoraMejorada {
         balanceClienteActual = 0;
         return cantidadDeDineroADevolver;
     } 
+    
+    public int vaciarDineroDeLaMaquina(){
+        int dineroExtraido = totalDineroAcumulado + balanceClienteActual;
+        totalDineroAcumulado = 0;
+        balanceClienteActual = 0;
+        return dineroExtraido;
+    }
 }
